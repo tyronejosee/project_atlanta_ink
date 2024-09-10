f12b28e1907a3552c0fcdcd9d70121ab6a59da8f
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-09-10 14:40:50 -0300

    refactor(artists): update page detail

--- frontend/src/app/artists/[slug]/page.tsx
+++ frontend/src/app/artists/[slug]/page.tsx
@@ -36,63 +36,54 @@ export async function generateMetadata({ params }: { params: { slug: string } })
 
 export default async function ArtistDetailPage({ params }: Props) {
   const { slug } = params;
+  const [artist, tattoos] = await Promise.all([
+    getBySlug<IArtist>("artists", slug),
+    getTattoosByArtist<[]>(slug),
+  ]);
 
-  try {
-    const [artist, tattoos] = await Promise.all([
-      getBySlug<IArtist>("artists", slug),
-      getTattoosByArtist<[]>(slug),
-    ]);
+  if (!artist) return notFound()
+  if (!tattoos) return notFound()
 
-    if (!artist) { return notFound(); }
-    if (!tattoos) { return notFound(); }
-
-    return (
-      <section className="bg-neutral-dark max-w-screen-xl mx-auto flex mt-16 min-h-screen">
-        <section className="w-96 flex flex-col p-4 space-y-4">
-          <Image
-            src={artist.image}
-            alt={artist.name}
-            width={300}
-            height={300}
-            className="w-full rounded-full mb-4"
-          />
-          <div>
-            <h1 className="text-4xl font-bold mb-2">{artist.name}</h1>
-            <p className="text-sm text-neutral-gray line-clamp-4">{artist.description}</p>
-          </div>
-          <div>
-            <h2 className="text-2xl font-semibold">Styles:</h2>
-            <ul className="flex space-x-1">
-              {artist.styles.map((style) => (
-                <li key={style.id}>
-                  <Badge>{style.name}</Badge>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="flex justify-center">
-            {artist.instagram && (
-              <a href={artist.instagram} target="_blank" rel="noopener noreferrer">
-                <Instagram />
-              </a>
-            )}
-            {artist.whatsapp && (
-              <a href={`https://wa.me/${artist.whatsapp}`} target="_blank" rel="noopener noreferrer">
-                <YouTube />
-              </a>
-            )}
-          </div>
-        </section>
-        <section className="w-full p-4">
-          <ParallaxScroll images={tattoos} />
-        </section>
+  return (
+    <section className="bg-neutral-dark max-w-screen-xl mx-auto flex mt-16 min-h-screen">
+      <section className="w-96 flex flex-col p-4 space-y-4">
+        <Image
+          src={artist.image}
+          alt={artist.name}
+          width={300}
+          height={300}
+          className="w-full rounded-full mb-4"
+        />
+        <div>
+          <h1 className="text-4xl font-bold mb-2">{artist.name}</h1>
+          <p className="text-sm text-neutral-gray line-clamp-4">{artist.description}</p>
+        </div>
+        <div>
+          <h2 className="text-2xl font-semibold">Styles:</h2>
+          <ul className="flex space-x-1">
+            {artist.styles.map((style) => (
+              <li key={style.id}>
+                <Badge>{style.name}</Badge>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="flex justify-center">
+          {artist.instagram && (
+            <a href={artist.instagram} target="_blank" rel="noopener noreferrer">
+              <Instagram />
+            </a>
+          )}
+          {artist.whatsapp && (
+            <a href={`https://wa.me/${artist.whatsapp}`} target="_blank" rel="noopener noreferrer">
+              <YouTube />
+            </a>
+          )}
+        </div>
       </section>
-    );
-  } catch (error) {
-    return (
-      <main className="bg-neutral-dark max-w-screen-xl mx-auto flex mt-16">
-        <p>Error load</p>
-      </main>
-    );
-  }
+      <section className="w-full p-4">
+        <ParallaxScroll images={tattoos} />
+      </section>
+    </section>
+  );
 }
