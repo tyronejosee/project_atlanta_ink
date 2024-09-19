1426931f58a9dbdb48fa11c616b359299e2a3a24
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-09-19 12:31:38 -0300

    feat(applicants): add structure to applicants page

---
+++ frontend/src/app/(routes)/applicants/apply/page.tsx
@@ -0,0 +1,77 @@
+import Image from "next/image";
+import { Input, Button, Textarea, Checkbox } from "@nextui-org/react";
+
+export default function ApplyPage() {
+  return (
+    <section className="max-w-screen-xl mx-auto mt-16">
+      <div className="grid grid-cols-2 gap-4 p-4">
+        <form className="space-y-4 px-12">
+          {/* <HeaderSection title="Apply for job" /> */}
+          <Input
+            label="Name"
+            size="sm"
+            type="text"
+            radius="md"
+            required
+            // {...register("firstName", { required: "First name is required" })}
+          />
+          <Input
+            label="Email"
+            size="sm"
+            type="email"
+            radius="md"
+            required
+            // {...register("firstName", { required: "First name is required" })}
+          />
+          <Input
+            label="Phone"
+            size="sm"
+            type="phone"
+            radius="md"
+            required
+            // {...register("firstName", { required: "First name is required" })}
+          />
+          <Textarea
+            label="Message"
+            size="sm"
+            radius="md"
+            // {...register("notes", { required: "Notes are required" })}
+          />
+          {/* {errors.notes?.message && (
+            <FormError>* {errors.notes?.message}</FormError>
+          )} */}
+          <div>
+            <input
+              type="file"
+              className="block w-full text-xs text-neutral-gray file:mr-4 file:py-2 file:px-4 file:rounded-xl file:border-0 file:text-sm file:font-semibold file:bg-neutral-darkgrey file:text-primary hover:file:text-neutral-light hover:file:bg-primary"
+              // {...register("file")}
+            />
+          </div>
+          <Checkbox
+            className="block"
+            // {...register("hasWorkInProgress")}
+          >
+            I wish to receive emails in case of a hiring process.
+          </Checkbox>
+          <Button
+            type="submit"
+            color="primary"
+            radius="md"
+            className="font-medium w-full"
+          >
+            Create Booking
+          </Button>
+        </form>
+        <section className="overflow-hidden rounded-xl">
+          <Image
+            src="/images/apply-background.webp"
+            alt="Apply Background"
+            width={1080}
+            height={1080}
+            className="object-cover w-full h-full"
+          />
+        </section>
+      </div>
+    </section>
+  );
+}
--- frontend/src/config/constants.ts
+++ frontend/src/config/constants.ts
@@ -34,6 +34,7 @@ export const NAV_ITEMS = [
   { id: 1, href: "/artists", label: "Artists" },
   { id: 2, href: "/prices", label: "Prices" },
   { id: 1, href: "/products", label: "Products" },
+  { id: 1, href: "/applicants/apply", label: "Applicants" },
 ];
 
 export const MENU_ITEMS = [
