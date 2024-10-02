b9d5ed9fdd62961c746e6bfc86d942801cc73b82
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-10-02 14:18:37 -0300

    chore(seo, assets): add open graph metadata and images

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -22,13 +22,29 @@ import {
 } from "@/lib/api";
 
 export const metadata: Metadata = {
-  title: "Atlanta Ink - Tattoo Studio ",
+  title: "Atlanta Ink - Tattoo Studio",
   description:
     "Welcome to Atlanta Ink, your premier tattoo studio. We specialize in custom tattoo designs, ensuring every piece is unique and tailored to your vision. Book your session today!",
   keywords:
     "atlanta ink, tattoo studio, custom tattoos, tattoo designs, tattoo artists, atlanta",
+  openGraph: {
+    title: "Atlanta Ink - Tattoo Studio",
+    description:
+      "Welcome to Atlanta Ink, your premier tattoo studio. We specialize in custom tattoo designs, ensuring every piece is unique and tailored to your vision. Book your session today!",
+    url: "https://atlanta-ink-studio.vercel.app/",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dwyvfa5dj/image/upload/v1727889010/Atlanta%20Ink%20API/atlanta-ink-seo.webp",
+        width: 1280,
+        height: 720,
+        alt: "Atlanta Ink Seo",
+      },
+    ],
+    siteName: "Atlanta Ink",
+  },
 };
 
+// https://res.cloudinary.com/dwyvfa5dj/image/upload/v1727889010/Atlanta%20Ink%20API/atlanta-ink-seo.webp
 export default async function HomePage() {
   const [services, tattoos, artists, prices, productsData, faqs] =
     await Promise.all([
